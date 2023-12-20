d2343582750f8565f08351e036518d3c732f34da
Alberto Lima <alberto.btlima@outlook.com>
2023-12-20 18:04:20 -0300

    Update JS

---
+++ 02-JavaScript-Intermediario/01-escopo-no-javascript/script.js
@@ -0,0 +1,33 @@
+/* if (true) {
+    const mensagem = 'Olá';
+    console.log(mensagem);
+}
+console.log(mensagem); */
+
+/* {
+    var mensagem = 'Olá';
+    console.log(mensagem); 
+}
+console.log(mensagem);  */
+
+/* function falar() {
+    var mensagem = 'Olá mundo!';
+    console.log(mensagem);
+}
+falar();
+console.log(mensagem); */
+
+let numero = 123;
+
+function falar() {
+    var mensagem = 'Olá mundo!';
+    console.log(mensagem);
+    function falarOutraCoisa() {
+
+    }
+    falarOutraCoisa();
+    console.log(falarOutraCoisa);
+}
+falar();
+console.log(falarOutraCoisa);
+console.log(numero);
